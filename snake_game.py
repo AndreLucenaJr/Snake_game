0b2af9eead2a06cea4362755ef8148c0cb392211
AndreLucenaJr <andrelucenajr123@gmail.com>
2023-09-11 14:02:23 -0300

    Draw the snake and create the moves events

--- snake_game.py
+++ snake_game.py
@@ -9,12 +9,20 @@ pygame.init()
 #set the game screen
 width, height = 400, 400
 screen = pygame.display.set_mode((width, height))
-screen.fill((110, 110, 5))
+
 pygame.display.set_caption('Snake Game')
 pygame.display.update()
 
 #Create the snake
+x, y = 200, 200
+delta_X, delta_y = 0, 0
 
+def snake():
+    global x, y
+    x = x + delta_X
+    y = y + delta_y
+    pygame.draw.rect(screen, (255, 255, 255), [x, y, 10, 10])
+    pygame.display.update()
 
 
 #set the events
@@ -22,4 +30,21 @@ while True:
     for event in pygame.event.get():
         #set the Quit event
         if event.type == QUIT:
-            quit()
+            quit() 
+        #Set the move events of the snake
+        if (event.type == pygame.KEYDOWN):
+            if(event.key == pygame.K_LEFT):  
+                delta_x = -10
+                delta_y = 10
+            elif(event.key == pygame.K_RIGHT):
+                delta_x = 10
+                delta_y = 0
+            elif(event.key == pygame.K_UP):
+                delta_x = 0
+                delta_y = -10
+            elif(event.key == pygame.K_DOWN):
+                delta_x = 0
+                delta_y = 10
+            else:
+                continue
+    snake()
